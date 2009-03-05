45cb2789650744c21b5d63db27c8e79a9caf644e
Yegor.Yarko <Yegor.Yarko@fc027d59-cf41-0410-a13e-92ad4256a95e>
2009-03-05 10:00:18 +0000

    Move to appropriate directory (trunk)

---
+++ src/jetbrains/buildserver/groovyPlug/GroovyReferencePropertiesProvider.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.buildserver.groovyPlug;
+
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.serverSide.parameters.AbstractBuildParameterReferencesProvider;
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.serverSide.SRunningBuild;
+import jetbrains.buildServer.serverSide.SBuildType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Date;
+
+/**
+ * @author Yegor.Yarko
+ *         Date: 04.03.2009
+ */
+public class GroovyReferencePropertiesProvider extends AbstractBuildParameterReferencesProvider {
+  private static final Logger LOG = Logger.getInstance(GroovyReferencePropertiesProvider.class.getName());
+
+  GroovyReferencePropertiesProvider() {
+    LOG.info("GroovyReferencePropertiesProvider initialized.");
+  }
+
+  @NotNull
+  @Override
+  public Map<String, String> getParameters(@NotNull SBuild build) {
+    final Map<String, String> result = new HashMap<String, String>();
+    addBuildStartTime(result, build);
+    addRunParameters(result, build);
+    return result;
+  }
+
+  private void addRunParameters(Map<String, String> buildParametersToAdd, SBuild build) {
+    SBuildType buildType = build.getBuildType();
+    if (buildType == null) return;
+
+    for (Map.Entry<String, String> runParameter : buildType.getRunParameters().entrySet()) {
+      buildParametersToAdd.put("runParam." + runParameter.getKey(), runParameter.getValue());
+    }
+  }
+
+  void addBuildStartTime(Map<java.lang.String, java.lang.String> buildParametersToAdd, SBuild build) {
+    Date buildStartTime = build.getStartDate();
+    Util.addDateTimeProperty(buildParametersToAdd, buildStartTime, "yyyyMMdd", "build.start.date", "BUILD_START_DATE");
+    Util.addDateTimeProperty(buildParametersToAdd, buildStartTime, "HHmmss", "build.start.time", "BUILD_START_TIME");
+  }
+
+}
---
+++ src/jetbrains/buildserver/groovyPlug/GroovyScriptsPreparer.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildserver.groovyPlug;
+
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.util.FileUtil;
+import jetbrains.buildServer.serverSide.ServerPaths;
+
+import java.io.File;
+
+/**
+ * @author Yegor.Yarko
+ *         Date: 19.01.2009
+ */
+public class GroovyScriptsPreparer {
+  private static final Logger LOG = Logger.getInstance(GroovyScriptsPreparer.class.getName());
+
+  private ServerPaths myServerPaths;
+  private String mySourceResourceDir;
+  private String myTargetDirUnderConfig;
+  private String[] myFileNames;
+
+  public GroovyScriptsPreparer(ServerPaths serverPaths) {
+    myServerPaths = serverPaths;
+  }
+
+  public void init() {
+    final String targetDir = new File(myServerPaths.getConfigDir(), myTargetDirUnderConfig).getAbsolutePath();
+    new File(targetDir).mkdirs();
+    for (String fileName : myFileNames) {
+      copyResource(mySourceResourceDir, targetDir, fileName);
+    }
+  }
+
+  private void copyResource(String sourceDir, String targetDir, String fileName) {
+    final File targetFile = new File(targetDir, fileName);
+    if (!targetFile.exists()) {
+      FileUtil.copyResource(getClass(), sourceDir + "/" + fileName, targetFile);
+      LOG.info("Created default script: " + targetFile.getAbsolutePath());
+    }
+  }
+
+  public void setSourceResourceDir(String mySourceFileInResources) {
+    this.mySourceResourceDir = mySourceFileInResources;
+  }
+
+  public void setTargetDirUnderConfig(String myTargetDirUnderConfig) {
+    this.myTargetDirUnderConfig = myTargetDirUnderConfig;
+  }
+
+  public void setFileNames(String[] fileNames) {
+    this.myFileNames = fileNames;
+  }
+}
